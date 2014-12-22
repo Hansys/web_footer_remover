c484043da0880bc979d33b7ed2bd9c791915e31c
Hansys <hans.balis@gmail.com>
2014-12-22 11:25:51 +0000

    Initiele Commit

---
+++ models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class web_footer_remover(models.Model):
+#     _name = 'web_footer_remover.web_footer_remover'
+
+#     name = fields.Char()
